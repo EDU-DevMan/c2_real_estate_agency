da1112e9b9ea989fb4914813ab8dcbab649314d6
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-07-22 11:51:23 +0300

    Add model field Flat.new_building

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,9 @@ from .models import Flat
 
 class SearchAdmin(admin.ModelAdmin):
     search_fields = ('owner','town','address')
+    readonly_fields = ['created_at']
 
 
 admin.site.register(Flat, SearchAdmin)
+
 
